f93cfe279e781374cfbf2a028ddd8ad68e9a19d4
Jonhvmp <jonhpaz08@gmail.com>
2024-04-02 14:11:11 -0300

    Update variaveis.php

--- Aulas/Introducao-php/variaveis.php
+++ Aulas/Introducao-php/variaveis.php
@@ -4,4 +4,7 @@ $mensagem = 'Helo World';
 $Mensagem = '';
 $MENSAGEM = '';
 
-//mesmo sendo semelhantes, existem diferença entre elas, e essa diferença faz com quê cada uma seja á própria variável
+print($mensagem);
+?>
+
+<!--mesmo sendo semelhantes, existem diferença entre elas, e essa diferença faz com quê cada uma seja á própria variável-->
